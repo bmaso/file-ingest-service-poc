dab3746aafc9355956af5b066b2444322ef8fa88
Brian Maso <brian@blumenfeld-maso.com>
2020-05-21 11:23:44 -0700

    File ingest event processor impl first pass complete

--- cluster-node/src/main/scala/cli/MainCLI.scala
+++ cluster-node/src/main/scala/cli/MainCLI.scala
@@ -1,18 +1,17 @@
 package bmaso.file_ingest_service_poc.cluster_node
 
 /**
- * Akka Cluster node which publishes a single behavior able to
- * fulfill the mesaging protocol defined by `ActorRef[FileIngestion.Order]`.
+ * Akka Cluster node which joins cluster sharding file ingestion entities and
+ * file cleansing and uploading stream processors.
  *
- * This particular implementation of the begavior utilizes:
- * * Akka Persistence to ensure actor failure and recovery will continue the various
- *   file ingestion workflows where they left off.
- * * Some DB or longer-term persistence in order to record the status of
- *   individual file ingestion so that the same file cannot be re-ingested
- *   multiple times.
+ * CLI options:
+ * * `--http-port <port>` -- server opens HTTP port for HTTP requests (see overall project documentation
+ *   for exact HTTP/JSON protocol. Each node hosts HTTP services, and must use a different port.
+ * * `--cluster-port <port>` -- indicates which port this node uses for cluster communications. Each node must use a
+ *   different port.
+ * * `--host-h2-database-server` -- if present, this node will host an H2 TCP database server. This is a convenience
+ *   in case the operator does not want to run a stand-alone SQL server in a separate process. Useful only for testing
+ *   purposes; a more robust DB setup should be employed in production deployments.
  **/
 object MainCLI {
-  def main(args: Array[String]): Unit = {
-    println("/glancing angsty, brooding and side-ways at the world...")
-  }
 }
--- cluster-node/src/main/scala/read_model/file-ingestion-event-processor.scala
+++ cluster-node/src/main/scala/read_model/file-ingestion-event-processor.scala
@@ -1,26 +1,90 @@
 package bmaso.file_ingest_service_poc.cluster_node
 
-import scala.concurrent.ExecutionContext
-import scala.concurrent.Future
 import akka.Done
 import akka.actor.typed.ActorSystem
-import akka.actor.typed.eventstream.EventStream
+import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.persistence.typed.PersistenceId
 import bmaso.akka.event_processor.EventProcessorStream
+import bmaso.file_ingest_service_poc.protocol.{FileIngestion => FileIngestion_protocol}
+import cats.data.NonEmptyList
 
-class FileIngestionEventProcessorStream(
-                                        system: ActorSystem[_],
+import scala.util.{Failure, Success}
+import scala.concurrent.{ExecutionContext, Future, Promise}
+
+class FileIngestionEventProcessorStream(system: ActorSystem[_],
                                         executionContext: ExecutionContext,
                                         eventProcessorId: String,
                                         tag: String)
   extends EventProcessorStream[FileIngestionEntity.Event](system, executionContext, eventProcessorId, tag) {
 
-  def processEvent(event: FileIngestionEntity.Event, persistenceId: PersistenceId, sequenceNr: Long): Future[Done] = {
-    log.info(s"FileIngestionEventProcessor($tag) consumed $event from $persistenceId with seqNr $sequenceNr")
+  /**
+   * For now we mock file cleansing using a future that always completes successfully in 15 seconds. The future value
+   * is the "cleansed" file name.
+   */
+  def cleanseFileAsync(dataFile: String): Future[String] = {
+    val p = Promise[String]
 
-    // TBD: When file enqueued, asynchronously start cleanse process
-    //   TBD: When cleanse finishes successfully, send FileIngestion.CleanseCompleteAcknowledgementOrder
-    //   TBD: When cleanse finishes in failure, send FileIngestion.ProblemsAcknowledgementOrder
+    executionContext.execute(() => {
+      log.trace(s"...beginning cleansing of file $dataFile")
+      Thread.sleep(15000)
+      p.complete(Success(dataFile + "/cleansed"))
+      log.trace(s"...completed cleansing of file $dataFile")
+    })
+
+    p.future
+  }
+
+  /**
+   * For now we mock file uploading using a future that always completes successfully in 5 seconds. The future value
+   * is just a unitary value.
+   */
+  def uploadFileAsync(file: String): Future[Unit] = {
+    val p = Promise[Unit]
+
+    executionContext.execute(() => {
+      log.trace(s"...beginning uploading of file $file")
+      Thread.sleep(5000)
+      p.complete(Success(()))
+      log.trace("...completed upload of file $file")
+    })
+
+    p.future
+  }
+
+  def processEvent(event: FileIngestionEntity.Event, persistenceId: PersistenceId, sequenceNr: Long): Future[Done] = {
+    lazy val entityRef = ClusterSharding(system).entityRefFor(FileIngestionEntity.EntityKey, persistenceId.id)
+
+    log.debug(s"FileIngestionEventProcessor($tag) consumed $event from $persistenceId with seqNr $sequenceNr")
+
+    event match {
+      case FileIngestionEntity.FileIngestionEnqueuedEvent(originalOrder, timestamp) =>
+        val cleanse_fut = cleanseFileAsync(originalOrder.dataFile)
+        cleanse_fut.onComplete ({
+          case Success(cleansedFile) =>
+            log.info(s"Completed cleansing of file ${originalOrder.dataFile} => ${cleansedFile}")
+            entityRef ! FileIngestion_protocol.CleanseCompleteAcknowledgementOrder(cleansedFile)
+
+          case Failure(exception) =>
+            log.warn("Problem cleansing file $originalOrder.dataFile", exception)
+            entityRef ! FileIngestion_protocol.ProblemsAcknowledgementOrder(NonEmptyList(exception.getMessage, List.empty))
+        })
+
+      case FileIngestionEntity.FileCleanseCompleteEvent(cleanseCompleteOrder, timestamp) =>
+        val upload_fut = uploadFileAsync(cleanseCompleteOrder.cleansedDataFile)
+        upload_fut.onComplete ({
+          case Success(_) =>
+            log.info(s"Completed uploading of file ${cleanseCompleteOrder.cleansedDataFile}")
+            entityRef ! FileIngestion_protocol.UploadCompleteAcknowledgementOrder()
+
+          case Failure(exception) =>
+            log.warn("Problem uploading file $originalOrder.dataFile", exception)
+            entityRef ! FileIngestion_protocol.ProblemsAcknowledgementOrder(NonEmptyList(exception.getMessage, List.empty))
+        })
+
+      case _  =>
+        //...all other events are ignorable
+        log.debug(s"Processor ignoring event $event")
+    }
 
     // TBD: When file cleansed, asynchronously start upload process
     //   TBD: When upload finishes successfully, send FileIngestion.UploadCompleteAcknowledgementOrder
