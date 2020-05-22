25783477fa0aac711c8179a252d22196c234a69f
Brian Maso <brian@blumenfeld-maso.com>
2020-05-22 07:15:18 -0700

    Removed 'web-server' subproject

--- web-server/src/main/scala/cli/MainCLI.scala
+++
@@ -1,7 +0,0 @@
-package bmaso.file_ingest_service_poc.web_server
-
-object MainCLI {
-  def main(args: Array[String]): Unit = {
-    println("/glancing angsty, brooding and side-ways at the world...")
-  }
-}
